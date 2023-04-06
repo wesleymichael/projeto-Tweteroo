9ec1b831c9f7e0c4c5acdc83c797f96b7f431e4b
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-04-06 14:58:01 -0300

    Add post function to sign-up route for adding new users

--- src/app.js
+++ src/app.js
@@ -2,22 +2,21 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 const PORT = 5000;
 
-const user = {
-    username: "",
-    avatar: "",
-}
-
-const tweet = {
-    username: user.username,
-    tweet: "",
-}
+const users = [];
+const tweet = [];
 
 app.post("/sign-up", (req, res) => {
-    res.send("rota criada");
+    const {username, avatar} = req.body;
+    const newUser = { username, avatar };
+
+    users.push(newUser);
+    
+    res.send("OK");
 });
 
 app.post("/tweets", (req, res) => {
