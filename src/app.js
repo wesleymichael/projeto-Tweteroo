0652351bd064d77ed1427844b2045db81e8cbff9
wesleymichael <wesleymichaelpereira@hotmail.com>
2023-04-06 15:13:23 -0300

    Add post function to '/tweets' route for adding new tweets

--- src/app.js
+++ src/app.js
@@ -8,19 +8,25 @@ app.use(cors());
 const PORT = 5000;
 
 const users = [];
-const tweet = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const {username, avatar} = req.body;
-    const newUser = { username, avatar };
-
-    users.push(newUser);
-    
+    //const newUser = { username, avatar };
+    users.push( { username, avatar } );
     res.send("OK");
 });
 
 app.post("/tweets", (req, res) => {
-    res.send("rota criada");
+    const {username, tweet} = req.body;
+    const isRegistered = users.some( (t) => t.username === username );
+
+    if(!isRegistered){
+        res.status(401).send("UNAUTHORIZED");
+        return;
+    }
+    tweets.push( {username, tweet} );
+    res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
